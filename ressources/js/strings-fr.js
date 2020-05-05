71a9a1c0118488e76731054791221835be69d1a2
PhilippeCanova <canova.philippe@meteo.fr>
2020-05-05 08:54:19 +0200

    Sorte de rebase pour sauver les fichiers de Steph

---
+++ ressources/js/strings-fr.js
@@ -0,0 +1,215 @@
+$_version = "Version";
+$_menu3_title1 = 'Raccourcis géographique';
+$_menu3_title2 = 'Vent';
+$_menu3_title3 = 'Précipitations';
+$_menu3_title4 = 'Nuages';
+$_menu3_title5 = 'Températures';
+$_menu3_title6 = 'Aérologie';
+$_menu_preferences = "Préférences"
+$_layer = "paramètre";
+$_previsions = "Prévisions";
+$_observations = "Observations"
+$_layer_uv_10m = 'Vent à 10m';
+$_layer_uv_alt = 'Vent en altitude';
+$_layer_rafale = 'Rafales à 10m (kt)';
+$_layer_opacityLabel = 'Opacité';
+$_layer_elevationLabel = 'Niveau';
+$_layer_t = 'Température';
+$_layer_td = 'Td 2m';
+$_layer_rrtt = 'Précipitations totales';
+$_layer_ngtt = 'Précipitations neigeuses totales cumulées';
+$_layer_visi_metro = 'Visibilité';
+$_layer_plafond_metro = 'Plafond';
+$_layer_sat_isp = 'Image Satellite Prévue';
+$_layer_nebul_bas = 'Nébul bas';
+$_layer_nebul_moyen = 'Nébul moyen';
+$_layer_nebul_haut = 'Nébul haut';
+$_coupe_terrain = "Coupe terrain";
+$_coupe_trajet = "Coupe trajet";
+$_parametres = "Paramètres";
+$_choix_language = "Choisissez la langue";
+$_francais = "Français";
+$_anglais = "Anglais";
+$_layer_radar_fr = "Radar";
+$_layer_foudre_fr = "Foudre";
+$_imagerie = "Imagerie observée";
+
+$_Dimanche = "Dimanche";
+$_Lundi = "Lundi";
+$_Mardi = "Mardi";
+$_Mercredi = "Mercredi";
+$_Jeudi = "Jeudi";
+$_Vendredi = "Vendredi";
+$_Samedi = "Samedi";
+
+$_Dimanche_bref = "D";
+$_Lundi_bref = "L";
+$_Mardi_bref = "M";
+$_Mercredi_bref = "M";
+$_Jeudi_bref = "J";
+$_Vendredi_bref = "V";
+$_Samedi_bref = "S";
+
+$_Dimanche_short = "Dim";
+$_Lundi_short = "Lun";
+$_Mardi_short = "Mar";
+$_Mercredi_short = "Mer";
+$_Jeudi_short = "Jeu";
+$_Vendredi_short = "Ven";
+$_Samedi_short = "Sam";
+
+$_Janvier = "Janvier";
+$_Fevrier = "Février";
+$_Mars = "Mars";
+$_Avril = "Avril";
+$_Mai = "Mai"
+$_Juin = "Juin";
+$_Juillet = "Juillet";
+$_Aout = "Aout";
+$_Septembre = "Septembre";
+$_Octobre = "Octobre";
+$_Novembre = "Novembre";
+$_Decembre = "Decembre";
+
+$_Janvier_short = "Janv";
+$_Fevrier_short = "Févr";
+$_Mars_short = "Mars";
+$_Avril_short = "Avri";
+$_Mai_short = "Mai"
+$_Juin_short = "Juin";
+$_Juillet_short = "Juil";
+$_Aout_short = "Aout";
+$_Septembre_short = "Sept";
+$_Octobre_short = "Octo";
+$_Novembre_short = "Nove";
+$_Decembre_short = "Dece";
+
+$_fr = "Métropole ";
+$_re = "Réunion";
+$_my = "Mayotte";
+$_ga = "Guadeloupe";
+$_ma = "Martinique";
+$_gy = "Guyane";
+$_po = "Polynésie francaise";
+$_nc = "Nouvelle Calédonie";
+$_sp = "Saint Pierre et miquelon";
+$_choix_geo = "Choisissez votre localisation";
+$_choix_repere = "Choisissez vos repères";
+$_repere_dep = "Départements";
+$_repere_front = "Frontieres";
+$_repere_grat = 'Quadrillages';
+$_repere_aero = 'Aeroports';
+$_label_choix_run_arome = "Choix de la date du modele Arome";
+$_label_choix_run_arpege = "Choix de la date du modele Arpege";
+$_analyse_generale = "Analyse";
+$_t_layer_arpege = "Temperature";
+$_z_layer_arpege = "Géopotentiel";
+$_no_new_modele = "Pas de nouveau modele disponible."
+$_nouveau_modele_arome_dispo = "Nouveau modele Arome disponible";
+$_nouveau_modele_arpege_dispo = "Nouveau modele Arpege disponible";
+$_depart = "Départ";
+$_etape = "Etape";
+$_arrivee = "Arrivée";
+$_coupe_trajet_vide = "Pour commencer selectionnez des points sur la carte. Ou un trajet déja enregistré.";
+$_coupe_terrain_vide = "Pour commencer selectionnez un point sur la carte. Ou une coupe terrain déja enregistrée.";
+
+$_valider_le_trajet = "Valider le trajet";
+$_valider_le_terrain = "Valider la coupe terrain";
+
+$_valider = "Valider";
+$_annuler = "Annuler";
+$_aucun_trajet = "Aucun trajet enregistré";
+$_aucun_terrain = "Aucune coupe terrain enregistrée";
+
+$_hu = "Humidité";
+$_rflctvt = "Précipitations";
+$_vv2 = "Vitesse Vert. (m.s-1)";
+$_iso_m10 = "Iso -10°";
+$_iso_0 = "Iso 0°";
+$_iso_p5 = "Iso +5°";
+$_theta = "Theta";
+$_tke = "TKE";
+$_hcl = "Hauteur Couche Limite (m)";
+$_uv_alt = "Vent altitude";
+$_t = "Température";
+$_choix_parametre_trajet = "Choisissez vos parametres";
+$_choix_parametre_terrain = "Choisissez vos parametres";
+$_choix_trajet = "Choisissez un trajet";
+$_choix_terrain = "Choisissez une coupe terrain";
+$_donne_nom_trajet = "Donnez un nom à ce trajet";
+$_donne_nom_terrain = "Donnez un nom à cette coupe terrain";
+$_liste_trajets_enregistres = "Liste des trajets enregistrés";
+$_liste_terrains_enregistres = "Liste des coupe terrains enregistrées";
+$_trajet = "Trajet";
+$_terrain = "Coupe terrain";
+$_niveau_de_vol = "Niveau de vol maximum :";
+$_N = "N";
+$_S = "S";
+$_E = "E";
+$_O = "O";
+$_symbol_degre = " ";
+$_terrain = "Terrain";
+$_du = "du";
+$_au = "au"
+$_title_hires = '<b>Prévisions modèle maille fine </b>';
+$_title_hires_notice = '(notice sur les champs)';
+$_title_polynesie_notice = 'Légende';
+$_title_imgobs_france = 'Imagerie observée France';
+$_title_imgobs_europe = 'Imagerie observée Europe';
+$_title_imgobs_polynesie = 'Imagerie observée Polynésie';
+$_title_zt = 'Carte d\'analyse générale Z/T';
+$_title_path = '<b>Coupe trajet</b>';
+$_title_path_notice = '(notice sur les champs)';
+$_title_vav = '<b>Aérologie dynamique (coupes pré-définies / relief)</b>';
+$_title_vav_notice = '(notice sur les champs)';
+$_title_terrain = '<b>Coupe terrain</b>';
+$_title_terrain_notice = '(notice sur les champs)';
+$_title_sigmets = '<b>Sigmets </b>';
+$_title_sigmets2 = '<b>Sigmets à # </b>';
+
+
+$_layer_hcl = 'Hauteur Couche Limite (m)';
+$_layer_vv2 = 'Vitesse Vert. (m.s-1)';
+$_layer_sat_ir_fr = 'Sat IR HRV';
+$_layer_sat_ir = 'Sat IR';
+$_layer_sat_vis = 'Sat visible';
+$_layer_radar = 'Radar';
+$_layer_foudre = 'Foudre';
+$_layer_z = 'Géopotentiel';
+
+
+
+$_layer_hu = 'Humidité';
+$_layer_iso_0 = 'Iso 0°';
+$_layer_iso2_0 = 'Iso 0° (2)';
+$_layer_iso_m10 = 'Iso -10°';
+$_layer_iso_p5 = 'Iso +5°';
+
+$_layer_uv_alt = 'Vent altitude';
+$_layer_t = 'Température';
+$_layer_rflctvt = 'Précipitations';
+$_layer_tke = 'TKE';
+$_layer_theta = 'Theta';
+$_layer_vv2 = 'Vitesse Vert. (m.s-1)';
+$_layer_hcl = 'Hauteur Couche Limite (m)';
+$_layer_firs = 'Firs';
+$_layer_airports = 'Aéroports';
+$_layer_sigmets = 'Sigmets';
+
+$_layer_baseLabel = 'Fonds de carte';
+
+
+$_layer_overlay_departement = 'Départements';
+$_layer_overlay_boundary = 'Frontières';
+$_layer_overlay_graticule = 'Quadrillage';
+$_layer_overlay_airport = 'Aéroports';
+
+$_loading = 'Génération';
+$_error_title = 'Erreur !';
+$_error_text = 'Une erreur est survenue, cliquer <a href="">ici</a> pour réessayer.';
+$_from = 'Heure de début : ';
+$_delay = 'Durée : ';
+$_fl = 'Niveau maximal de vol : ';
+$_submit = 'Valider';
+$_open_section = 'Ouvrir la coupe';
+$_averts_sigmets = '<center><h3>AVERTISSEMENT</h3><h4>Produit Expérimental</h4>Ne se substitue pas à la consultation des SIGMETS dans la rubrique Messages.<br><br>SIGMET VA et TC non représentés dans cet affichage.'
